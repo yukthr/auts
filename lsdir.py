67a10f24a1e6767a4c9317d99ea1947bf082caa6
Ubuntu <ubuntu@ip-172-31-95-179.ec2.internal>
2018-04-05 19:48:27 +0000

    adding various changes

--- lsdir.py
+++ lsdir.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
 #l = os.system("ls | egrep -i sample")
-proc = subprocess.Popen('ls | egrep -i sample',stdout=subprocess.PIPE)
+proc = subprocess.Popen('ls | egrep -i sample',stdout=subprocess.PIPE,shell=True)
 output = proc.stdout.read()
-print output
+directories = output.split()
+for i in directories:
+	print "Program Start"
+	os.system("python {}/*.py".format(i))
+
