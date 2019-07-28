e2da9db2342c130ac5a04193acf614baf3cc40e5
Rakesh <28902301+yukthr@users.noreply.github.com>
2019-07-29 00:59:09 +0100

    Update flask_server_new.py

--- random_programs/home_proj/soil_moisture/flask_server_new.py
+++ random_programs/home_proj/soil_moisture/flask_server_new.py
@@ -3,13 +3,39 @@ from aws_pubsub import publish_aws
 app = Flask(__name__)
 import ast # will be used to convert byte strings(in this dictionary is enclosed in string) to evaluate to dictionary
 import json
+from datetime import datetime
+from influxdb import InfluxDBClient
+
+
+def write_to_influx(dvalue):
+    current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    client = InfluxDBClient('192.168.0.151',8086)
+    client.switch_database('moisture_value')
+    json_body = [
+        {
+            "measurement": "moisture_value",
+            "tags": {
+            "name":"moisture_value_basil_bedroom",
+            },
+            "time": current_time,
+            "fields":
+                dvalue,
+        },]
+    client.write_points(json_body)
+    print("writtent to influx {}".format(dvalue))
 
 @app.route("/moisture", methods=['GET','POST'])
 def moisture_publish():
 
     req_data = request.get_data()
     dict_value = ast.literal_eval(req_data.decode("utf-8")) #We get byte string from the Microcontroller
-    publish_aws(json.dumps(dict_value)) #We convert this into Json here instead of converting in AWS script
+    print(dict_value)
+    try:
+        write_to_influx(dict_value)
+        publish_aws(json.dumps(dict_value)) #We convert this into Json here instead of converting in AWS script
+    except:
+        write_to_influx(dict_value)
+        publish_aws(json.dumps(dict_value))
     return "aws message has now been published"
 
 """
@@ -23,4 +49,4 @@ Dockerising flaks was not a problem at all, the problem was with AWS Pubsub, pro
 
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
