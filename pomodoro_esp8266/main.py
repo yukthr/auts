4e3d3386d0da2b3918830364106844c769ae8d92
Rakesh <raaki.88@gmail.com>
2019-08-03 22:11:19 +0100

    adding changes

--- pomodoro_esp8266/main.py
+++ pomodoro_esp8266/main.py
@@ -3,14 +3,12 @@ def do_connect():
     import network
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    #wlan.ifconfig(('192.168.1.82','255.255.255.0','192.168.1.1','89.101.160.4'))
     if not wlan.isconnected():
         print('connecting to network...')
         try:
-            wlan.connect('R-VMVMVM', 'Himaja@1992')
+            wlan.connect('R-VMVMVM', 'xxxx')
         except:
             pass
-            #wlan.connect('R-VMVMVM','Himaja@1992')
     print('network config:', wlan.ifconfig())
 
 do_connect()
