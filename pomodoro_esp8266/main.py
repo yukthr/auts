eb12392449d10de0c955f507b6b8052bf818a8e5
Rakesh <raaki.88@gmail.com>
2019-08-03 21:55:03 +0100

    adding files

--- pomodoro_esp8266/main.py
+++ pomodoro_esp8266/main.py
@@ -1,18 +1,17 @@
-
-import time
+from pomodoro import pomodoro_timer
 def do_connect():
     import network
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
+    #wlan.ifconfig(('192.168.1.82','255.255.255.0','192.168.1.1','89.101.160.4'))
     if not wlan.isconnected():
         print('connecting to network...')
         try:
-            wlan.connect('R-VMVMVM', '')
-            if '0.0.0.0' in wlan.ifconfig():
-                wlan.connect('R-VMVMVM', '')
-        except: 
-            wlan.connect('R-VMVMVM','')
+            wlan.connect('R-VMVMVM', 'Himaja@1992')
+        except:
+            pass
+            #wlan.connect('R-VMVMVM','Himaja@1992')
     print('network config:', wlan.ifconfig())
 
 do_connect()
-import pomodoro
+pomodoro_timer()
--- pomodoro_esp8266/pomodoro.py
+++ pomodoro_esp8266/pomodoro.py
@@ -10,12 +10,10 @@ import urequests
 i2c = I2C(-1, scl=Pin(5), sda=Pin(4))
 
 
-def  post_to_slack(message):
-  
+def post_to_slack(message):
+    url = "https://hooks.slack.com/services/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"
 
-    url = "your slack url here"
-
-    payload = "{\"text\":\""+message+"\"}"
+    payload = "{\"text\":\"" + message + "\"}"
     headers = {
         'Content-type': "application/json",
     }
@@ -23,50 +21,38 @@ def  post_to_slack(message):
 
 
 def pomodoro_timer():
-    for j in range(5):
-        if j <=5:
+    for j in range(6):
+        if j < 5:
             for i in range(25):
-                if i <=25:
-                    oled_width = 128
-                    oled_height = 64
-                    oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
-                    wri = Writer(oled, courier, verbose=False)
-                    Writer.set_textpos(oled, 14, 0) # In case a previous test has altered this
-                    wri.printstring('{} Mins '.format(25-i))
-                    wri.printstring('\n{}/5 Iters'.format(j))
-                    oled.show()
-                    time.sleep(60)
-                    i = i + 1
-                else:
-                    for k in range(5):
-                        if k <= 5:
-                            post_to_slack("5m Break")
-                            oled_width = 128
-                            oled_height = 64
-                            oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
-                            wri = Writer(oled, courier, verbose=False)
-                            Writer.set_textpos(oled, 14, 0)  # In case a previous test has altered this
-                            wri.printstring('{} Break '.format(5 - k))
-                            oled.show()
-                            time.sleep(60)
-                            k = k + 1
-                            post_to_slack("25M  start")
-
-            j=j+1
-
+                oled_width = 128
+                oled_height = 64
+                oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+                wri = Writer(oled, courier, verbose=False)
+                Writer.set_textpos(oled, 14, 0)  # In case a previous test has altered this
+                wri.printstring('{} Mins '.format(25 - i))
+                wri.printstring('\n{}/5 Iters'.format(j+1))
+                oled.show()
+                time.sleep(60)
+            for k in range(5):
+                oled_width = 128
+                oled_height = 64
+                oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+                wri = Writer(oled, courier, verbose=False)
+                Writer.set_textpos(oled, 14, 0)  # In case a previous test has altered this
+                wri.printstring('{} Break '.format(5 - k))
+                oled.show()
+                if k == 0:
+                    post_to_slack("5M Break")
+                time.sleep(60)
         else:
+            post_to_slack("15M Break")
             for l in range(15):
-                if l <= 15:
-                    post_to_slack("15m Break start")
-                    oled_width = 128
-                    oled_height = 64
-                    oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
-                    wri = Writer(oled, courier, verbose=False)
-                    Writer.set_textpos(oled, 14, 0)  # In case a previous test has altered this
-                    wri.printstring('{} 15 Break '.format(15 - l))
-                    oled.show()
-                    time.sleep(60)
-                    l = l + 1
-
-pomodoro_timer()
-machine.reset()
+                oled_width = 128
+                oled_height = 64
+                oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+                wri = Writer(oled, courier, verbose=False)
+                Writer.set_textpos(oled, 14, 0)  # In case a previous test has altered this
+                wri.printstring('{} Break '.format(15 - l))
+                oled.show()
+                time.sleep(60)
+    machine.reset()
