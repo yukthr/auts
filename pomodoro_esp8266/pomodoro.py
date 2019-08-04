21cef096c3c6cac4743e8a9be5409d18ea611cab
rakesh-telnyx <rakesh@telnyx.com>
2019-08-04 19:17:38 +0100

    Adding few changes

--- pomodoro_esp8266/pomodoro.py
+++ pomodoro_esp8266/pomodoro.py
@@ -41,13 +41,9 @@ def pomodoro_timer():
                 Writer.set_textpos(oled, 14, 0)  # In case a previous test has altered this
                 wri.printstring('{} Break '.format(5 - k))
                 oled.show()
-                if k == 0:
-                    post_to_slack("5M Break")
                 time.sleep(60)
         else:
             for l in range(15):
-                if l == 0:
-                    post_to_slack("15M Break!")
                 oled_width = 128
                 oled_height = 64
                 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
