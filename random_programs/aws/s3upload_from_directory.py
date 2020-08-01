d98b6d71a6f246c782b818c15795bb5916bf0a9f
Rakesh <28902301+yukthr@users.noreply.github.com>
2020-08-01 13:47:14 +0100

    Update s3upload_from_directory.py

--- random_programs/aws/s3upload_from_directory.py
+++ random_programs/aws/s3upload_from_directory.py
@@ -6,21 +6,18 @@ import time
 
 os.chdir('/root/images/IOS')
 
-
-
 list_of_files = glob.glob('*')
 s3uri = 's3://evengrakesh'
 
 start = time.perf_counter()
 
-
 with concurrent.futures.ThreadPoolExecutor() as executor:
 
         results = [executor.submit(s3upload, file_name, s3uri) for file_name in list_of_files]
-
         for f in concurrent.futures.as_completed(results):
                 print(f.result())
 
+                
 finish = time.perf_counter()
 print(finish-start)
 
